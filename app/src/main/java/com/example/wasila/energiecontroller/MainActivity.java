4617eee3a00346322182ce8eca05449d78d749ba
firchtn <firaschaabencss@gmail.com>
2017-05-05 15:50:05 +0100

    add action for invalid username and password

--- app/src/main/java/com/example/wasila/energiecontroller/MainActivity.java
+++ app/src/main/java/com/example/wasila/energiecontroller/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static String user = "root";
@@ -20,13 +21,20 @@ public class MainActivity extends AppCompatActivity {
 
 
         public void sendMessage (View view){
+            TextView msgv1 = (TextView) findViewById(R.id.msglogin);
+
             EditText text1 = (EditText) findViewById(R.id.username);
             String userName = text1.getText().toString();
             EditText text2 = (EditText) findViewById(R.id.password);
             String passWord = text2.getText().toString();
+            System.out.println(userName);
+            System.out.println(passWord);
             if ( userName.equals(user) && passWord.equals(pass)) {
                 Intent intent = new Intent(this, SendMessageActivity.class);
                 startActivity(intent);
+            }
+            else{
+                msgv1.setText("Username or password invalid ! ");
             }
 
     }
